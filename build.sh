fc3ba67ed1501cffa2982e973605e56d153226a5
Shahab Dogar <shahab96@gmail.com>
2018-03-30 08:10:00 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
+# You may need to run chmod +x on this file before it can be executed.
+
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./prod/crud ./src/
 docker-compose up --build -d
